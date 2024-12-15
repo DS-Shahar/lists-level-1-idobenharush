9492390a8f0335e64f29a3929bb7c555603c8675
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2024-12-15 11:48:51 +0200

    Update ex11.java

--- src/ex11.java
+++ src/ex11.java
@@ -13,3 +13,20 @@ public static Node<Integer> removeCommon(Node<Integer> listA, Node<Integer> list
     
     return head.getNext();
 }
+public static Node<Integer> removeCommon2(Node<Integer> listA, Node<Integer> listB) {
+    Node<Integer> current = new Node<>(0, listA);
+    Node<Integer> head = current;
+    Node<Integer> pB = listB;
+    while (current.hasNext()) {
+        int a = current.getNext().getValue();
+        pB = listB;
+        while (pB != null && !pB.getValue().equals(a)))
+            pB = pB.getNext();
+        }
+        if (pB != null)
+            current.setNext(current.getNext().getNext());
+        else
+            current = current.getNext();        
+    }    
+    return head.getNext();
+}
